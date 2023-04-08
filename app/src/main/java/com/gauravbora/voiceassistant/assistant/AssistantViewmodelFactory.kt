776ab73ef827183171d174e62acba2f7e6d714d2
Gaurav Singh Bora <gauravbora263642@gmail.com>
2023-04-08 23:15:45 +0530

    project 1 - Voice Assistant for Visually Impaired

---
+++ app/src/main/java/com/gauravbora/voiceassistant/assistant/AssistantViewmodelFactory.kt
@@ -0,0 +1,24 @@
+package com.gauravbora.voiceassistant.assistant
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.gauravbora.voiceassistant.data.AssistantDao
+import java.lang.IllegalArgumentException
+
+class AssistantViewmodelFactory (
+    private val dataSource: AssistantDao,
+    private val application:Application) :ViewModelProvider.Factory
+
+{
+    @Suppress("UNCHECKED_CAST")
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(AssistantViewmodel::class.java)){
+            return AssistantViewmodel(dataSource,application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel  class")
+    }
+
+
+}
